7fe64f5d9bd630e842168ec8591ddc8f38f26666
scepticae <samuel_leonard@comcast.net>
2020-08-05 18:19:26 -0400

    Built HTML, CSS and API

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,16 @@
-console.log("Testing");
+// Get Quote from API
+async function getQuote(){
+    const proxyURL = 'https://cors-anywhere.herokuapp.com/';
+    const apiURL = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    try {
+        const response = await fetch(proxyURL + apiURL);
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        getQuote();
+        console.log("Whoops! No Quote.", error);
+    }
+}
+
+// On Load
+getQuote();
