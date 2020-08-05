d4c517bc4c06844c96829703c7982b21782289cd
scepticae <samuel_leonard@comcast.net>
2020-08-05 19:24:34 -0400

    Added Change Author and Quote Functionality

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,9 @@
+const guoteContainer = document.getElementById('');
+const guoteText = document.getElementById('quote');
+const guoteAuthor = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 // Get Quote from API
 async function getQuote(){
     const proxyURL = 'https://cors-anywhere.herokuapp.com/';
@@ -5,10 +11,19 @@ async function getQuote(){
     try {
         const response = await fetch(proxyURL + apiURL);
         const data = await response.json();
-        console.log(data);
+        if (data.quoteText.length > 120) {
+            quoteText.classList.add('long-quote');
+        } else {
+            quoteText.classList.remove('long-quote');
+        }
+        quoteText.innerText = data.quoteText;
+        if (data.quoteAuthor === '') {
+            quoteAuthor.innerText = 'Unknown Author';
+        } else {
+            quoteAuthor.innerText = data.quoteAuthor; 
+        }  
     } catch (error) {
         getQuote();
-        console.log("Whoops! No Quote.", error);
     }
 }
 
